990d0ce0fae0722a29d03962930059b691584fc8
Bluck009 <113055490+Bluck009@users.noreply.github.com>
2023-09-11 14:43:45 -0400

    first

--- TP_BlackJack/Joueur.cs
+++ TP_BlackJack/Joueur.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace TP_BlackJack
 {
-    public abstract class Joueur
+    public abstract class Joueur 
     {
-        String nom;
-        String email;
-        int argent;
-        int valeurMisee;
-        int positionTable;
-        List<Carte> main;
+        private String nom;
+        private String email;
+        private int argent;
+        private int valeurMisee;
+        private int positionTable;
+        private List<Carte> main;
 
         public String Nom
         {
@@ -26,6 +26,69 @@ namespace TP_BlackJack
                 }
                 nom = value;
             }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Le courriel ne peut pas être vide, nulle ou contenir des espaces");
+                }
+                email = value;
+            }
+        }
+        public int Argent
+        {
+            get { return argent; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("No money, no play!");
+                }
+                argent = value;
+            }
+        }
+        public int ValeurMisee
+        {
+            get { return valeurMisee; }
+            set {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("La mise ne peut pas être négative");
+                }
+                valeurMisee = value;
+            }
+        }
+        public int PositionTable
+        {
+            get { return positionTable; }
+            set { positionTable = value; }
+        }
+
+        public List<Carte> Main
+        {
+            get { return main; }
+            set { main = value; }
+        }
+        public Joueur( String p_nom, String p_email)
+        {
+            this.Nom = p_nom;
+            this.Email = p_email;
+        }
+        public int CalculerNimbreDePoints()
+        {
+            int nombrePoint = 0;
+            return nombrePoint;
+        }
+
+        public virtual int CompareTo(Joueur p_joueur)
+        {
+            int argentDiff = 0;
+            return argentDiff;
         }
     }
 
